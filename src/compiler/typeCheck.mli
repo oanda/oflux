5ef1e804544d250963149ca24aea3584fd1bfa12
Mark Pichora <mark@oanda.com>
2012-07-10 22:00:01 -0400

    licensing
    AGPLv3

--- src/compiler/typeCheck.mli
+++ src/compiler/typeCheck.mli
@@ -1,3 +1,20 @@
+(*
+ *    OFlux: a domain specific language with event-based runtime for C++ programs
+ *    Copyright (C) 2008-2012  Mark Pichora <mark@oanda.com> OANDA Corp.
+ *
+ *    This program is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Affero General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Affero General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Affero General Public License
+ *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *)
 (** Basic type checking for maintaining consequences of 
     the type unification that are done when semantically analyzing 
     an OFlux program *)
